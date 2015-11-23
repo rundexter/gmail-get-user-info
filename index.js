b0d49439cc04b604f4c58ced3e84635fdfd2a2c9
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-11-23 19:39:37 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+var _ = require('lodash'),
+  google = require('googleapis'),
+  OAuth2 = google.auth.OAuth2;
+
 module.exports = {
+    checkAuthOptions: function (step, dexter) {
+
+        if(!step.input('userId').first()) {
+
+            this.fail('A userId input variable is required for this module');
+        }
+
+        if(!dexter.environment('access_token')) {
+
+            this.fail('A access_token environment variable is required for this module');
+        }
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +23,17 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        this.checkAuthOptions(step, dexter);
+
+        var oauth2Client = new OAuth2();
+        oauth2Client.setCredentials({access_token: dexter.environment('access_token'), refresh_token: dexter.environment('refresh_token')});
+
+        google.options({ auth: oauth2Client });
+        google.gmail('v1').users.getProfile(_.merge({auth: oauth2Client}, step.inputs()), function (err, message) {
+
+            err? this.fail(err) : this.complete(message);
+        }.bind(this));
+
     }
 };
