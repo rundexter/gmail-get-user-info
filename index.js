554c9ad940db67f796f02416ff07ad113d2e1863
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-21 15:39:07 +0200

    fix Oauth

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var _ = require('lodash'),
-  google = require('googleapis'),
-  OAuth2 = google.auth.OAuth2;
+    util = require('./util.js'),
+    google = require('googleapis'),
+    service = google.gmail('v1');
+
+var pickInputs = {
+        'userId': { key: 'userId', validate: { req: true } }
+    },
+    pickOutputs = {
+        'emailAddress': 'emailAddress',
+        'messagesTotal': 'messagesTotal',
+        'threadsTotal': 'threadsTotal',
+        'historyId': 'historyId'
+    };
 
 module.exports = {
     checkAuthOptions: function (step, dexter) {
@@ -23,17 +34,23 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
+        var OAuth2 = google.auth.OAuth2,
+            oauth2Client = new OAuth2(),
+            credentials = dexter.provider('google').credentials();
+        var inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs);
 
-        this.checkAuthOptions(step, dexter);
+        if (validateErrors)
+            return this.fail(validateErrors);
 
-        var oauth2Client = new OAuth2();
-        oauth2Client.setCredentials({access_token: dexter.environment('google_access_token'), refresh_token: dexter.environment('google_refresh_token')});
-
+        // set credential
+        oauth2Client.setCredentials({
+            access_token: _.get(credentials, 'access_token')
+        });
         google.options({ auth: oauth2Client });
-        google.gmail('v1').users.getProfile(_.merge({auth: oauth2Client}, step.inputs()), function (err, message) {
+        service.users.getProfile(inputs, function (error, message) {
 
-            err? this.fail(err) : this.complete(message);
+            error? this.fail(error) : this.complete(util.pickOutputs(message, pickOutputs));
         }.bind(this));
-
     }
 };
